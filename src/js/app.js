4d4de7c4d20b98a2e05d025a11f848251e18da19
Igor Konovodov <konovodov@list.ru>
2024-10-04 22:44:37 +0300

    add: sectionNavigation, sectionAside (former part 'navigation' of header), discount; adaptation of the desktop version has begun

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,30 @@ const slider = new Splide("#slider", {
 });
 
 slider.mount();
+
+let countdown;
+const timerDisplay = document.querySelector(".discount__countdown");
+
+const timer = (seconds) => {
+  const currentTime = Date.now();
+  const endTime = currentTime + seconds * 1000;
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((endTime - Date.now()) / 1000);
+    if (secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+    displayTimer(secondsLeft);
+  }, 1000);
+};
+
+const displayTimer = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+
+  const display = `${minutes} : ${
+    remainderSeconds < 10 ? "0" : ""
+  }${remainderSeconds}`;
+  timerDisplay.textContent = display;
+};
